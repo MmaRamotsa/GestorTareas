43863afa44be046733338ce4ebf022f0b7201c3d
MmaRamotsa <ma.martinez68@gmail.com>
2017-04-19 14:24:36 +0200

    Inicio de la aplicación de gestión de tareas. MAMG

---
+++ js/codigo.js
@@ -0,0 +1,15 @@
+(function(){
+ // Initializa Firebase
+  var config = {
+    apiKey: "AIzaSyDyhTlFeQUOn8DpH9rvjSkbilSNgV7z2Bk",
+    authDomain: "gestor-de-tareas-c2032.firebaseapp.com",
+    databaseURL: "https://gestor-de-tareas-c2032.firebaseio.com",
+    projectId: "gestor-de-tareas-c2032",
+    storageBucket: "gestor-de-tareas-c2032.appspot.com",
+    messagingSenderId: "198899505899"
+  };
+  firebase.initializeApp(config);
+
+
+  $("addTarea").on("click", )
+});
